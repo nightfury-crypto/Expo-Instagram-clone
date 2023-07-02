785df22288e80a260e4b9b5af0b42680dcec2d1c
Karnail Singh Choudhary <pradhankarnail@gmail.com>
2023-07-02 11:39:16 +0530

    tempdatafile
    Signed-off-by: Karnail Singh Choudhary <pradhankarnail@gmail.com>

---
+++ tempdata/data.js
@@ -0,0 +1,59 @@
+const postData = [
+    {
+      id: "1",
+      username: "indian_army_",
+      postImg:
+        "https://images.news18.com/ibnlive/uploads/2020/01/Members-of-Shwet-Ashw.jpg?impolicy=website&width=0&height=0",
+      userPfp:
+        "https://wallpapers.com/images/featured/indian-army-logo-9yyeqs1cn57bvbtt.jpg",
+      likes: 3400,
+      comments: 1003,
+      caption:
+        " Flag ceremony was organised by #INSTunir and the Indian army jawans are showing their talent by riding and posing on a bike waving Indian National Flag.  Flag ceremony was organised by #INSTunir and the Indian army jawans are showing their talent by riding and posing on a bike waving Indian National Flag. Flag ceremony was organised by #INSTunir and the Indian army jawans are showing their talent by riding and posing on a bike waving Indian National Flag. Flag ceremony was organised by #INSTunir and the Indian army jawans are showing their talent by riding and posing on a bike waving Indian National Flag. Flag ceremony was organised by #INSTunir and the Indian army jawans are showing their talent by riding and posing on a bike waving Indian National Flag. Flag ceremony was organised by #INSTunir and the Indian army jawans are showing their talent by riding and posing on a bike waving Indian National Flag. Flag ceremony was organised by #INSTunir and the Indian army jawans are showing their talent by riding and posing on a bike waving Indian National Flag. Flag ceremony was organised by #INSTunir and the Indian army jawans are showing their talent by riding and posing on a bike waving Indian National Flag. Flag ceremony was organised by #INSTunir and the Indian army jawans are showing their talent by riding and posing on a bike waving Indian National Flag.",
+    },
+    {
+      id: "2",
+      username: "ig_toothless__",
+      postImg:
+        "https://images.pexels.com/photos/1933873/pexels-photo-1933873.jpeg?auto=compress&cs=tinysrgb&w=600",
+      userPfp:
+        "https://images.pexels.com/photos/927437/pexels-photo-927437.jpeg?auto=compress&cs=tinysrgb&w=600",
+      likes: 956,
+      comments: 382,
+      caption: " play like a game changer. Do your bests",
+    },
+    {
+      id: "3",
+      username: "nature.love",
+      postImg:
+        "https://images.pexels.com/photos/551622/pexels-photo-551622.jpeg?auto=compress&cs=tinysrgb&w=600",
+      userPfp:
+        "https://images.pexels.com/photos/927437/pexels-photo-927437.jpeg?auto=compress&cs=tinysrgb&w=600",
+      likes: 56,
+      comments: 20,
+      caption: " nature ❣️",
+    },
+    {
+        id: "4",
+        username: "checkmate",
+        postImg:
+          "https://cdn.pixabay.com/photo/2016/02/22/09/44/chess-1215079_640.jpg",
+        userPfp:
+          "https://images.pexels.com/photos/129742/pexels-photo-129742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        likes: 5677,
+        comments: 2098,
+        caption: "checkmate",
+      },
+  ];
+
+  
+  const storiesData = [
+    {id:"1", username:"indian_army_", userPfp:"https://wallpapers.com/images/featured/indian-army-logo-9yyeqs1cn57bvbtt.jpg", storyImg:"https://images.news18.com/ibnlive/uploads/2020/01/Members-of-Shwet-Ashw.jpg?impolicy=website&width=0&height=0", storyTime:"3min", storySeen: true}, 
+    {id:"2", username:"ig_toothless__", userPfp:"https://images.pexels.com/photos/927437/pexels-photo-927437.jpeg?auto=compress&cs=tinysrgb&w=600", storyImg:"https://images.unsplash.com/photo-1562408845-207640039f07?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80", storyTime:"3h", storySeen: false},
+    {id:"3", username:"nature.love", userPfp:"https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg", storyImg:"https://images.unsplash.com/photo-1604311795833-25e1d5c128c6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80", storyTime:"20h", storySeen: false},
+    {id:"4", username:"checkmate", userPfp:"https://images.pexels.com/photos/129742/pexels-photo-129742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", storyImg:"https://images.unsplash.com/photo-1584294311015-1bda86d6824d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80", storyTime:"1h", storySeen: true},
+  ];
+
+  const tagsData = null
+  
+export { postData, storiesData, tagsData}
